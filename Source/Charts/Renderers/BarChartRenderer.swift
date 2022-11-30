76c2145705f82ca855a02b4ade1857cb33de48d9
Ovidiu D. Nițan <nitanovidiu@gmail.com>
2022-11-30 11:47:24 +0200

    Modify BarChartRenderer with rounded caps

--- Source/Charts/Renderers/BarChartRenderer.swift
+++ Source/Charts/Renderers/BarChartRenderer.swift
@@ -379,7 +379,9 @@ open class BarChartRenderer: BarLineScatterCandleBubbleRenderer
                 context.setFillColor(dataSet.color(atIndex: j).cgColor)
             }
             
-            context.fill(barRect)
+            let bezierPath = UIBezierPath(roundedRect: barRect, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: barRect.size.width/2.0, height: barRect.size.width/2.0))
+            context.addPath(bezierPath.cgPath)
+            context.drawPath(using: .fill)
             
             if drawBorder
             {
